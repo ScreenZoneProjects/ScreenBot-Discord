cb1f6b0a4364a521ac785b4a2cb778281f047e89
jbltx <gtmicka@hotmail.fr>
2017-08-25 21:41:21 -0400

    add env method in start script

--- start.sh
+++ start.sh
@@ -1,2 +1,3 @@
+#!/bin/sh
 echo "{\n\t\"EMAIL\":null,\n\t\"OWNER\":\"${OWNER_ID}\",\n\t\"PASSWORD\":null,\n\t\"PREFIXES\":[\"${COMMAND_PREFIX}\"],\n\t\"TOKEN\":\"${DISCORD_TOKEN}\",\n\t\"default\":\n\t{\n\t\t\"ADMIN_ROLE\":\"${ADMINS_NAME}\",\n\t\t\"MOD_ROLE\":\"${MODS_NAME}\",\n\t\t\"PREFIXES\":[]\n\t}\n}\n" > data/red/settings.json
 python3 launcher.py --start $*
